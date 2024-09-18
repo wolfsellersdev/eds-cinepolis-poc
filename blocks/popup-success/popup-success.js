c59775383e0a9b6dc9a6f369c603635eb322ae00
wolfsellersdev <wolfsellersdev@gmail.com>
2024-09-18 17:35:41 -0600

    Primeras correcciones de EDS

--- blocks/popup-success/popup-success.js
+++ blocks/popup-success/popup-success.js
@@ -1,8 +1,8 @@
 export default async function decorate(block) {
-    const button = block.querySelector('a');
-    button.addEventListener('click', () => {
-        const popup = document.querySelector( '[class="section form-container popup-success-container"]' );
-        popup.removeAttribute('style');
-    });
-  }
+  const button = block.querySelector('a');
+  button.addEventListener('click', () => {
+    const popup = document.querySelector( '[class="section form-container popup-success-container"]' );
+    popup.removeAttribute('style');
+  });
+}
   
