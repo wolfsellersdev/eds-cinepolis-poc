895f85f91e431fdf4658cfb87ed64ff05113b9a3
wolfsellersdev <wolfsellersdev@gmail.com>
2024-09-18 17:28:33 -0600

    Se han agregado las flechas para cambiar de card en el carrusel

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,1 +1,82 @@
-/* stylelint-disable no-empty-source */
+
+
+export default async function decorate(block) {
+    
+    const cardsToShow = 3;
+    const cards = [...block.children];
+    const parentElement = block.parentElement;
+    let index  = 0;
+    let startX = 0;
+    let startY = 0;
+    let distX  = 0;
+    let distY  = 0;
+    const threshold = 50; // Min distance to cosider a swipe
+    const restaint  = 100; // Max distance in the Y axis to consider a swipe
+    const allowedTime = 500; // Max time for a valid swipe
+    let startTime = 0;
+
+    
+    const renderCards = () => { 
+        block.innerHTML = '';
+        cards.slice(index, index + cardsToShow).forEach( e => {
+            block.appendChild(e);
+        });
+    }
+
+
+    if( 'ontouchstart' in window || navigator.maxTouchPoints > 0 ){
+        block.addEventListener( 'touchstart',  e => {
+            const touchObj = e.changedTouches[0];
+            startX = touchObj.pageX;
+            startY = touchObj.pageY;
+            startTime = new Date().getTime();
+            e.preventDefault();
+        }, false);
+
+        block.addEventListener( 'touchend', e => {
+            const touchObj = e.changedTouches[0];
+            distX = touchObj.pageX - startX;
+            distY = touchObj.pageY - startY;
+            const elapsedTime = new Date().getTime() - startTime;
+
+            if( elapsedTime <= allowedTime && Math.abs(distX) >= threshold && Math.abs(distY) <= restaint ) {
+                if( distX < 0 && (index + 1) <= (cards.length - cardsToShow)) {
+                    index++;
+                    renderCards();
+                } else if( distX > 0 && (index - 1) >= 0) {
+                    index--;
+                    renderCards();
+                }
+            }
+        });
+    }
+
+
+    const btnLeft = document.createElement('a');
+    btnLeft.innerText = '<-- ';
+    btnLeft.classList.add('prev-image');
+    btnLeft.addEventListener('click', () => {
+        if( index - 1 >= 0 ) {
+            index--;
+            renderCards();
+        }
+    });
+
+    const btnRight = document.createElement('a');
+    btnRight.innerText = ' -->';
+    btnRight.classList.add('next-image');
+    btnRight.addEventListener('click', () => {
+        if( index + 1 <= (cards.length - cardsToShow) ){
+            index++;
+            renderCards();
+        }
+    });
+
+    renderCards();
+
+    parentElement.appendChild(btnLeft);
+    parentElement.appendChild(btnRight);
+}
+
+
+  
--- blocks/popup-success/popup-success.js
+++ blocks/popup-success/popup-success.js
@@ -1,5 +1,4 @@
-
-  export default async function decorate(block) {
+export default async function decorate(block) {
     const button = block.querySelector('a');
     button.addEventListener('click', () => {
         const popup = document.querySelector( '[class="section form-container popup-success-container"]' );
