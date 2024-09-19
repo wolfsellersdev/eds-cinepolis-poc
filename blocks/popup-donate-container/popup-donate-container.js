d8957f89a90648677b48f1d3e544e27a8c3689d7
wolfsellersdev <wolfsellersdev@gmail.com>
2024-09-18 19:24:05 -0600

    Se han realizado ajustes de lint

--- blocks/popup-donate-container/popup-donate-container.js
+++ blocks/popup-donate-container/popup-donate-container.js
@@ -1,15 +1,14 @@
 export default async function decorate(block) {
   const parent = block.parentElement.parentElement;
   const button = document.querySelector('[data-action="/fomrulario-apoya-nuestra-causa"]')
-  .querySelector('button');
+    .querySelector('button');
   parent.children[0].querySelector('strong')
-  .addEventListener('click', () => {
-    parent.style.display = 'none';
-  });
+    .addEventListener('click', () => {
+      parent.style.display = 'none';
+    });
   button.addEventListener('click', (e) => {
     document.querySelector('[class="section popup-success-container"]')
-    .removeAttribute('style');
+      .removeAttribute('style');
     e.preventDefault();
   });
 }
-  
--- blocks/popup-success/popup-success.js
+++ blocks/popup-success/popup-success.js
@@ -1,6 +1,6 @@
 export default async function decorate(block) {
   block.children[0].children[1].querySelector('strong')
-  .addEventListener('click', () => {
-    block.parentElement.parentElement.style.display = 'none';
-  });
+    .addEventListener('click', () => {
+      block.parentElement.parentElement.style.display = 'none';
+    });
 }
